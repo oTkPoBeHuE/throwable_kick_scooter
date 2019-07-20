5bec4f6820f595d94a95bdf3277fb17356db8e96
Ivan Pakhotin <xxoTkPoBeHuExx@gmail.com>
2019-07-20 03:15:10 +0300

    Updates

---
+++ precache-manifest.6cd74dfb00dbd6ceb1b42cdb05fc5d83.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85e8230ee73b9391ca40338dc407820",
+    "url": "/throwable_kick_scooter/index.html"
+  },
+  {
+    "revision": "581b4fe4cbb6bfe62c7d",
+    "url": "/throwable_kick_scooter/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "581b4fe4cbb6bfe62c7d",
+    "url": "/throwable_kick_scooter/static/js/2.febb7274.chunk.js"
+  },
+  {
+    "revision": "9e8ca2c45b825131309b",
+    "url": "/throwable_kick_scooter/static/js/main.cc9454ed.chunk.js"
+  },
+  {
+    "revision": "d429c043f3e454a86760",
+    "url": "/throwable_kick_scooter/static/js/runtime~main.818c7699.js"
+  },
+  {
+    "revision": "94328b3c4777ccf3c012dc2c82633560",
+    "url": "/throwable_kick_scooter/static/media/scooter1.94328b3c.png"
+  },
+  {
+    "revision": "a57067ba57dd872d5d8d1d1a5069a7c3",
+    "url": "/throwable_kick_scooter/static/media/scooter2.a57067ba.png"
+  },
+  {
+    "revision": "881e01f80b1a002dd608e8959e4837d5",
+    "url": "/throwable_kick_scooter/static/media/scooter3.881e01f8.png"
+  },
+  {
+    "revision": "bd53729dcac6c57d75f5a46d860e949e",
+    "url": "/throwable_kick_scooter/static/media/scooter4.bd53729d.png"
+  },
+  {
+    "revision": "704480cb8978916575f646b6a6173f84",
+    "url": "/throwable_kick_scooter/static/media/scooter5.704480cb.png"
+  },
+  {
+    "revision": "bd9c8aa01583331350170aac2b917548",
+    "url": "/throwable_kick_scooter/static/media/scooter6.bd9c8aa0.png"
+  }
+]);
